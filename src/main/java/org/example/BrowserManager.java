8e5a487359a35280e6af6122e40cc6ddb36845f0
Shefali Darji <shefalidarji21@gmail.com>
2024-05-01 22:06:28 +0100

    first commit

---
+++ src/main/java/org/example/BrowserManager.java
@@ -0,0 +1,63 @@
+
+package org.example;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class BrowserManager extends Utils{
+
+    //creating obj to call browser from TestData
+    LoadProp loadProp = new LoadProp();
+
+
+    //method to start browser and type url
+    //String browser = "Chrome";
+
+    public void openBrowser(){
+    //if chrome it will enter this loop
+        if (loadProp.getProperty("browser").equalsIgnoreCase("Chrome")){
+
+            driver = new ChromeDriver();
+            System.out.println("Your browser is Chrome");
+
+}  //if firefox it will enter this loop
+        else if (loadProp.getProperty("browser").equalsIgnoreCase("Firefox")) {
+
+            driver = new FirefoxDriver();
+            System.out.println("Browser is Firefox");
+
+}//if edge it will enter this loop
+        else if (loadProp.getProperty("browser").equalsIgnoreCase("Edge")) {
+
+            driver = new EdgeDriver();
+            System.out.println("Browser is Edge");
+
+}//if safari it will enter this loop
+        else if (loadProp.getProperty("browser").equalsIgnoreCase("Safari")) {
+
+            driver = new SafariDriver();
+            System.out.println("Browser is Safari");
+
+}//if none of the above it will enter this loop i.e any other browser
+        else {
+            System.out.println("Your browser is " + loadProp.getProperty("browser"));
+        }
+
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS );
+        driver.get("https://demo.nopcommerce.com/");
+    }
+
+    //method to close the windows
+    public void closeBrowser(){
+
+        driver.quit();
+    }
+
+}
